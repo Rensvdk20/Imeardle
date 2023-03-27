8003267aa91d579ef7ab38ebb2013d1118041fcd
Rensvdk20 <rensvdk@gmail.com>
2023-03-27 21:29:01 +0200

    Added playlist api endpoints

--- pages/api/playlist.js
+++
@@ -1,6 +0,0 @@
-import prisma from "../../prisma/client";
-
-export default async function handler(req, res) {
-	const message = await prisma.playlist.findMany();
-	res.status(200).json({ message });
-}
---
+++ pages/api/playlist/index.js
@@ -0,0 +1,41 @@
+import prisma from "./../../../prisma/client";
+
+export default async function handler(req, res) {
+	const { method } = req;
+
+	switch (method) {
+		// Get all
+		case "GET":
+			try {
+				const playlist = await prisma.playlist.findMany({
+					include: {
+						Songs: true,
+					},
+				});
+
+				res.status(200).json(playlist);
+			} catch (error) {
+				res.status(500).json({ error: error.message });
+			}
+			break;
+
+		// Add new playlist
+		case "POST":
+			try {
+				const { name, coverUrl } = req.body;
+				const playlist = await prisma.playlist.create({
+					data: {
+						name,
+						coverUrl,
+					},
+				});
+				res.status(201).json(playlist);
+			} catch (error) {
+				res.status(500).json({ error: error.message });
+			}
+			break;
+		default:
+			res.setHeader("Allow", ["GET"]);
+			res.status(405).end(`Method ${method} Not Allowed`);
+	}
+}
