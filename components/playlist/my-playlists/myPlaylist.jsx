f304e1b2a1611a0a05f975632fb012db771f9b0d
Rensvdk20 <rensvdk@gmail.com>
2024-02-09 17:03:26 +0100

    Fixed playlist not found

--- components/playlist/my-playlists/myPlaylist.jsx
+++ components/playlist/my-playlists/myPlaylist.jsx
@@ -8,7 +8,7 @@ import { getMyPlaylistsAction } from "../actions";
 import { useState, useEffect } from "react";
 
 function MyPlaylist() {
-	const [playlists, setPlaylists] = useState([]);
+	const [playlists, setPlaylists] = useState({});
 
 	const loadAllPlaylists = async () => {
 		setPlaylists(await getMyPlaylistsAction());
@@ -24,35 +24,38 @@ function MyPlaylist() {
 				<h2>My Playlists</h2>
 				<div className="playlist-list">
 					<div className="row">
-						{playlists.status != 200 &&
-						playlists.data !== undefined ? (
-							<div>You don't have any playlists yet</div>
-						) : (
-							playlists.data?.map((playlist) => (
-								<div
-									className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3"
-									key={playlist.id}
-								>
-									<Link
-										href={`/manager/playlist/${playlist.id}`}
+						{playlists.status == 200 ? (
+							playlists.data.length > 0 ? (
+								playlists.data?.map((playlist) => (
+									<div
+										className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3"
+										key={playlist.id}
 									>
-										<div className="item">
-											<div className="item-cover">
-												<div className="item-cover-overlay"></div>
-												<Image
-													src={playlist.coverUrl}
-													alt="Playlist cover"
-													width={500}
-													height={250}
-												/>
+										<Link
+											href={`/manager/playlist/${playlist.id}`}
+										>
+											<div className="item">
+												<div className="item-cover">
+													<div className="item-cover-overlay"></div>
+													<Image
+														src={playlist.coverUrl}
+														alt="Playlist cover"
+														width={500}
+														height={250}
+													/>
+												</div>
+												<div className="item-name">
+													{playlist.name}
+												</div>
 											</div>
-											<div className="item-name">
-												{playlist.name}
-											</div>
-										</div>
-									</Link>
-								</div>
-							))
+										</Link>
+									</div>
+								))
+							) : (
+								<div>You don't have any playlists yet</div>
+							)
+						) : (
+							""
 						)}
 					</div>
 				</div>
